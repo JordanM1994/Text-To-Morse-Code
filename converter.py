88a9cb0179a020ee536d192e3e0b6953a9225edd
Jordan McLeod <jordandmcleod@outlook.com>
2022-04-24 07:51:52 +0100

    updated the comments within the code

--- converter.py
+++ converter.py
@@ -1,9 +1,15 @@
 import pandas
 
+# Placing the csv data into a dataframe
+
 alphabet = pandas.read_csv("morse_code_files/morse_code.csv")
 alphabet_dataframe = pandas.DataFrame(alphabet)
 
+# transforming the csv data into a dictionary
+
 morse_code_alphabet = {row.letter: row.morse_code for (index, row) in alphabet_dataframe.iterrows()}
+
+# Brain - asking for the users words, splitting them into letters and finding the corresponding morse code
 
 is_on = True
 
@@ -18,6 +24,7 @@ while is_on:
     else:
         is_on = False
 
+# takes the morse code out of a list and into a string below:
 code = ""
 
 for word in word_to_code:
